5570da29ddcd75d60ed636327d9e2bd950c2a5f8
Andrew Udell <andrewk.udell@gmail.com>
2014-09-11 15:26:14 -0400

    adding existing repo for thinkful

---
+++ Project-Lesson1-Storyboards/Project-Lesson1-Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project-Lesson1-Storyboards
+//
+//  Created by Andrew Udell on 9/9/14.
+//  Copyright (c) 2014 Thinkful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
